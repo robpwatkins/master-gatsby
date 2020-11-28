eb64876bfc8d03191dc021afd8a9a34e91800247
Rob W <robpwatkins@gmail.com>
2020-11-28 13:23:18 -0600

    yarp

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -5,6 +5,8 @@ import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 
+const heyoo = 'heyoo';
+
 export default function Layout({ children }) {
   return (
     <div>
